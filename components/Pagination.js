9b0afd6bda1b88a7bc10a7b4cd4f19a552540c37
iitsuraj <suraj.saini.phe17@itbhu.ac.in>
2021-05-25 11:34:29 +0530

    Seo page

--- components/Pagination.js
+++ components/Pagination.js
@@ -5,46 +5,45 @@ const Pagination = ({ pages, page }) => {
   // console.log("pages", pages, "page", page);
   let previousPage;
   let nextPage;
-
+  let previousPageLink;
+  let nextPageLink;
   if (page === 1 || pages === 1) {
     previousPage = "#";
     nextPage = "#";
+    previousPageLink = undefined;
+    nextPageLink = undefined;
   }
+
   if (page === 2) {
     previousPage = "/";
+    previousPageLink = "https://yogicscriptures.itsuraj.com";
   }
+
   if (page > 2) {
     previousPage = `/page/${page - 1}`;
+    previousPageLink = `https://yogicscriptures.itsuraj.com${previousPage}/`;
   }
+
   if (pages === page) {
     nextPage = "#";
+    nextPageLink = undefined;
   }
+
   if (page < pages) {
     nextPage = `/page/${page + 1}`;
+    nextPageLink = `https://yogicscriptures.itsuraj.com${nextPage}/`;
   }
+
   return (
     <div className="mt-8">
       <Head>
-        {previousPage !== "#" || previousPage !== "/" ? (
-          <link
-            rel="prev"
-            href={`https://yogicscriptures.itsuraj.com${previousPage}/`}
-          />
-        ) : null}
-        {nextPage !== "#" ? (
-          <link
-            rel="next"
-            href={`https://yogicscriptures.itsuraj.com${nextPage}/`}
-          />
-        ) : null}
+        {previousPageLink ? <link rel="prev" href={previousPageLink} /> : null}
+        {nextPageLink ? <link rel="next" href={nextPageLink} /> : null}
         {page !== 1 ? (
           <link
             rel="canonical"
             href={`https://yogicscriptures.itsuraj.com/page/${page}/`}
           />
-        ) : null}
-        {page === 2 ? (
-          <link rel="prev" href={`https://yogicscriptures.itsuraj.com`} />
         ) : null}
       </Head>
 
